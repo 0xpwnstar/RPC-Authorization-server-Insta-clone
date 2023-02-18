2ea52abf09ce2e5043bae385f7ba8537b08d65ab
0xpwnstar <chatna717@gmail.com>
2023-02-18 19:19:41 +0530

    saturday test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {JSONRPCServer} = require('json-rpc-2.0');
+const { JSONRPCServer } = require("json-rpc-2.0");
+const app = express();
+app.use(bodyParser.json());
 
 
 const server = new JSONRPCServer();
@@ -11,10 +13,6 @@ const server = new JSONRPCServer();
 // It can also return a promise of the result.
 server.addMethod("echo", ({ text }) => text);
 server.addMethod("log", ({ message }) => console.log(message));
-
-const app = express();
-app.use(bodyParser.json());
-
 app.post("/json-rpc", (req, res) => {
   const jsonRPCRequest = req.body;
   // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
